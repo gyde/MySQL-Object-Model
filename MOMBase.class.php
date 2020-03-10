e6bbc70bf8dfa8ea94b38c988783974531d77e80
Martin Gyde Poulsen <gyde@nqrd.dk>
2020-03-10 19:39:16 +0100

    Handle bool correctly, pdo escapes false => ''
    * Added test to confirm bug
    * Fixed casting in escapeObjectPair

--- MOMBase.class.php
+++ MOMBase.class.php
@@ -606,8 +606,9 @@ abstract class MOMBase implements \Serializable
 	  */
 	protected static function query($connection, $sql)
 	{
-		if (static::VERBOSE_SQL)
+		if (static::VERBOSE_SQL) {
 			error_log($sql);
+		}
 
 		try {
 			$result = $connection->query($sql, \PDO::FETCH_ASSOC);
@@ -621,27 +622,23 @@ abstract class MOMBase implements \Serializable
 	/**
 	  * Escape object value
 	  * Everything is escaped as strings except for NULL
-	  * @param string $field
-	  * @param string $type
-	  * @return string
 	  */
-	protected function escapeObjectPair($field, $type = 'varchar')
+	protected function escapeObjectPair(string $field, string $type = 'varchar') : string
 	{
-		if ($this->$field !== NULL)
-		{
-			if (strpos('int', $type) !== FALSE)
-				return '`'.$field.'` = '.(int)$this->$field;
-
-			return '`'.$field.'` = '.$this->escapeObject($this->$field);
-		}
-		else
+		if ($this->$field === NULL) {
 			return '`'.$field.'` = NULL';
+		}
+			
+		if (strpos($type, 'int') !== false) {
+			return '`'.$field.'` = '.(int)$this->$field;
+		}
+		
+		return '`'.$field.'` = '.$this->escapeObject($this->$field);
 	}
 
 	/**
 	  * Escape a value using static PDO connection
 	  * @param string $value
-	  * @param bool $quote wrap value in single quote
 	  * @return string
 	  */
 	protected static function escapeStatic($value)
@@ -677,11 +674,10 @@ abstract class MOMBase implements \Serializable
 
 	/**
 	  * Escape a value according to PDO connection
-	  * @param PDO $connection mysql connection
-	  * @param string $value
-	  * @return string
+	  * @param mixed $value
+	  * @return mixed
 	  */
-	private static function escape($connection, $value)
+	private static function escape(\PDO $connection, $value)
 	{
 		if (!$connection instanceof \PDO)
 			throw new BaseException(BaseException::MISSING_CONNECTION);
