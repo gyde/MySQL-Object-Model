a868ad6d0d9209f51fc33dddac93cc021495532b
Oinkling <zlug@nazg.dk>
2023-11-22 13:08:35 +0100

    Added depricated warnings to tests

--- src/Compound.php
+++ src/Compound.php
@@ -188,7 +188,7 @@ class Compound extends Base
       */
     private static function getRowByIdsStatic($ids)
     {
-        $sql = self::buildCompoundSql($ids, 'self::escapeStatic');
+        $sql = self::buildCompoundSql($ids, [self::class, 'escapeStatic']);
 
         $res = self::queryStatic($sql);
 
--- tests/autoload.php
+++ tests/autoload.php
@@ -1,5 +1,7 @@
 <?php
 
+error_reporting(E_ALL);
+
 require_once __DIR__ . '/../autoload.php';
 
 spl_autoload_register(function ($className) {
